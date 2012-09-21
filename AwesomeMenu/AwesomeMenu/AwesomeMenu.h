b851edef41c0a691b11a543a2a6688ce6257ed68
Ross Chapman <ross.chapman@me.com>
2012-09-21 14:30:01 -0400

    Converted to Arc

--- AwesomeMenu/AwesomeMenu/AwesomeMenu.h
+++ AwesomeMenu/AwesomeMenu/AwesomeMenu.h
@@ -14,36 +14,34 @@
 
 @interface AwesomeMenu : UIView <AwesomeMenuItemDelegate>
 {
-    NSArray *_menusArray;
     int _flag;
     NSTimer *_timer;
     AwesomeMenuItem *_addButton;
     
-    id<AwesomeMenuDelegate> _delegate;
     BOOL _isAnimating;
 }
 @property (nonatomic, copy) NSArray *menusArray;
 @property (nonatomic, getter = isExpanding) BOOL expanding;
-@property (nonatomic, assign) id<AwesomeMenuDelegate> delegate;
+@property (nonatomic, weak) id<AwesomeMenuDelegate> delegate;
 
-@property (nonatomic, retain) UIImage *image;
-@property (nonatomic, retain) UIImage *highlightedImage;
-@property (nonatomic, retain) UIImage *contentImage;
-@property (nonatomic, retain) UIImage *highlightedContentImage;
+@property (nonatomic, strong) UIImage *image;
+@property (nonatomic, strong) UIImage *highlightedImage;
+@property (nonatomic, strong) UIImage *contentImage;
+@property (nonatomic, strong) UIImage *highlightedContentImage;
 
-@property (nonatomic, assign) CGFloat nearRadius;
-@property (nonatomic, assign) CGFloat endRadius;
-@property (nonatomic, assign) CGFloat farRadius;
-@property (nonatomic, assign) CGPoint startPoint;
-@property (nonatomic, assign) CGFloat timeOffset;
-@property (nonatomic, assign) CGFloat rotateAngle;
-@property (nonatomic, assign) CGFloat menuWholeAngle;
-@property (nonatomic, assign) CGFloat expandRotation;
-@property (nonatomic, assign) CGFloat closeRotation;
+@property (nonatomic) CGFloat nearRadius;
+@property (nonatomic) CGFloat endRadius;
+@property (nonatomic) CGFloat farRadius;
+@property (nonatomic) CGPoint startPoint;
+@property (nonatomic) CGFloat timeOffset;
+@property (nonatomic) CGFloat rotateAngle;
+@property (nonatomic) CGFloat menuWholeAngle;
+@property (nonatomic) CGFloat expandRotation;
+@property (nonatomic) CGFloat closeRotation;
 
 - (id)initWithFrame:(CGRect)frame menus:(NSArray *)aMenusArray;
 @end
 
 @protocol AwesomeMenuDelegate <NSObject>
-- (void)AwesomeMenu:(AwesomeMenu *)menu didSelectIndex:(NSInteger)idx;
+- (void)awesomeMenu:(AwesomeMenu *)menu didSelectIndex:(NSInteger)idx;
 @end
--- AwesomeMenu/AwesomeMenu/AwesomeMenu.m
+++ AwesomeMenu/AwesomeMenu/AwesomeMenu.m
@@ -75,12 +75,6 @@ static CGPoint RotateCGPointAroundCenter(CGPoint point, CGPoint center, float an
     return self;
 }
 
-- (void)dealloc
-{
-    [_addButton release];
-    [_menusArray release];
-    [super dealloc];
-}
 
 #pragma mark - getters & setters
 
@@ -192,9 +186,9 @@ static CGPoint RotateCGPointAroundCenter(CGPoint point, CGPoint center, float an
         _addButton.transform = CGAffineTransformMakeRotation(angle);
     }];
     
-    if ([_delegate respondsToSelector:@selector(AwesomeMenu:didSelectIndex:)])
+    if ([_delegate respondsToSelector:@selector(awesomeMenu:didSelectIndex:)])
     {
-        [_delegate AwesomeMenu:self didSelectIndex:item.tag - 1000];
+        [_delegate awesomeMenu:self didSelectIndex:item.tag - 1000];
     }
 }
 
@@ -205,7 +199,6 @@ static CGPoint RotateCGPointAroundCenter(CGPoint point, CGPoint center, float an
     {
         return;
     }
-    [_menusArray release];
     _menusArray = [aMenusArray copy];
     
     
@@ -264,7 +257,7 @@ static CGPoint RotateCGPointAroundCenter(CGPoint point, CGPoint center, float an
         SEL selector = self.isExpanding ? @selector(_expand) : @selector(_close);
 
         // Adding timer to runloop to make sure UI event won't block the timer from firing
-        _timer = [[NSTimer timerWithTimeInterval:timeOffset target:self selector:selector userInfo:nil repeats:YES] retain];
+        _timer = [NSTimer timerWithTimeInterval:timeOffset target:self selector:selector userInfo:nil repeats:YES];
         [[NSRunLoop currentRunLoop] addTimer:_timer forMode:NSRunLoopCommonModes];
         _isAnimating = YES;
     }
@@ -277,7 +270,6 @@ static CGPoint RotateCGPointAroundCenter(CGPoint point, CGPoint center, float an
     {
         _isAnimating = NO;
         [_timer invalidate];
-        [_timer release];
         _timer = nil;
         return;
     }
@@ -320,7 +312,6 @@ static CGPoint RotateCGPointAroundCenter(CGPoint point, CGPoint center, float an
     {
         _isAnimating = NO;
         [_timer invalidate];
-        [_timer release];
         _timer = nil;
         return;
     }
